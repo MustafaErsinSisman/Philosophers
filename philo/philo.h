484dd1e466819a2e0c5b908e23901df612c53912
Mustafa Ersin Şişman <musisman@k1m37s01.42istanbul.com.tr>
2025-05-04 16:29:30 +0300

    struct yapısı

--- philo/philo.h
+++ philo/philo.h
@@ -19,10 +19,33 @@
 # include <sys/time.h>
 # include <unistd.h> 
 
-
-
 # define ERR_ARG "Argument error\n"
 
-void error(char *err);
+typedef struct s_info
+{
+	int philo_count;
+	int time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int must_eat_count;
+	int someone_died;
+	int start_time;
+	pthread_mutex_t *fork;
+	pthread_mutex_t print_lock;
+}t_info;
+
+typedef struct s_philo
+{
+	int id;
+	int eat_count;
+	long last_meal;
+	pthread_t thread;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+	t_info *info;
+}t_philo;
+
+int	error(char *err);
+int	check_arg(char **str);
 
 #endif
--- philo/utils.c
+++ philo/utils.c
@@ -24,8 +24,66 @@ static void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-void error(char *err)
+int	error(char *err)
 {
-        ft_putstr_fd(err, 2);
-        exit(1);
+	ft_putstr_fd(err, 2);
+	return (-1);
+}
+
+int	ft_isdigit(int c)
+{
+	if ((c >= 48 && c <= 57) || (c == '-' || c == '+'))
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	size_t	i;
+	size_t	nb;
+	int		sing;
+
+	i = 0;
+	sing = 1;
+	nb = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sing = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (str[i] - '0') + (nb * 10);
+		i++;
+	}
+	return (nb * sing);
+}
+
+int	check_arg(char **str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[++i])
+	{
+		j = -1;
+		while (str[i][++j])
+		{		
+			if (!ft_isdigit(str[i][j]))
+				return (-1);	
+		}
+	}
+	if (ft_atoi(str[1]) > 200)
+		return (-1);
+	i = 0;
+	while (str[++i])
+	{
+		if (ft_atoi(str[i]) <= 0)
+			return (-1);
+	}
+	return (0);
 }
