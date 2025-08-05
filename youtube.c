948969fa4b9238720ec679ab93a3857c35e17fdb
Mustafa Ersin Şişman <musisman@k1m22s06.42istanbul.com.tr>
2025-08-05 19:12:35 +0300

    basit thread fonksiyonu kullanımı öğrendim

--- youtube.c
+++ youtube.c
@@ -6,14 +6,25 @@
 /*   By: musisman <musisman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:41:26 by musisman          #+#    #+#             */
-/*   Updated: 2025/08/05 18:43:02 by musisman         ###   ########.fr       */
+/*   Updated: 2025/08/05 19:09:31 by musisman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "philo/philo.h"
 
-int main(int ac, char **av)
+void	*routine()
 {
-        
-        return 0;
+	printf("Test threads\n");
+        return (0);
+}
+
+int	main(int ac, char **av)
+{
+	pthread_t	t1;
+
+        (void)ac;
+        (void)av;
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	return (0);
 }
