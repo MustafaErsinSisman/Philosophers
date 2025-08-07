5ebee4211fac58cbe5f1659ef553b210af95cfb6
Mustafa Ersin Şişman - WSL <sismanmustafaersin@gmail.com>
2025-08-07 17:44:09 +0300

    thread oynatma listesi 5 viedo izlendi pthread_join için return value alma'da kalındı

---
+++ deneme.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: musisman <<musisman@student.42.fr>>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 14:41:37 by musisman          #+#    #+#             */
+/*   Updated: 2025/03/25 14:41:37 by musisman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DENEME_H
+# define DENEME_H
+
+# include <pthread.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h> 
+
+# include <sys/wait.h>
+# include <sys/types.h>
+
+#endif
--- youtube.c
+++ youtube.c
@@ -3,28 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   youtube.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: musisman <musisman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:41:26 by musisman          #+#    #+#             */
-/*   Updated: 2025/08/05 19:09:31 by musisman         ###   ########.fr       */
+/*   Updated: 2025/08/07 17:43:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo/philo.h"
+#include "deneme.h"
+
+int mails = 0;
+pthread_mutex_t mutex;
 
 void	*routine()
 {
-	printf("Test threads\n");
-        return (0);
+	size_t count = 10101010;
+	
+	for (size_t i = 0; i < count; i++)
+	{
+		pthread_mutex_lock(&mutex);
+		mails++;
+		pthread_mutex_unlock(&mutex);
+	}
+	
+	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	pthread_t	t1;
+    (void)ac;
+    (void)av;
 
-        (void)ac;
-        (void)av;
-	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_join(t1, NULL);
+	pthread_t	th[8];
+	int i = 0;
+	
+	pthread_mutex_init(&mutex, NULL);
+	while (i < 8)
+	{
+		if (pthread_create(&th[i++], NULL, &routine, NULL))
+		{
+			perror("Failed to creat thread\n");
+			return (1);
+		}
+		printf("thread %d: has started\n", i);
+	}
+	i = 0;
+	printf("\n");
+	while (i < 8)
+	{
+		if (pthread_join(th[i++], NULL))
+		{
+			perror("Failed to join thread\n");
+			return (2);
+		}
+		printf("thread %d: has joined\n", i);
+	}
+	pthread_mutex_destroy(&mutex);
+	printf("\nNumber of mails: %d\n", mails);
 	return (0);
 }
