0d72f2a58f7d9ee3eeb43e571bddca5f187ac875
Mustafa Ersin Şişman - WSL <sismanmustafaersin@gmail.com>
2025-08-10 16:53:41 +0300

    tester %60 geçti

--- philo_cld/monitor.c
+++ philo_cld/monitor.c
@@ -13,7 +13,7 @@ void	*monitor_routine(void *arg)
 		// Sonra ölüm kontrolü
 		if (check_death(data))
 			break ;
-		usleep(50);
+		usleep(100);
 	}
 	return (NULL);
 }
@@ -30,15 +30,15 @@ int	check_death(t_data *data)
 		pthread_mutex_lock(&data->meal_mutex);
 		if (current_time - data->philos[i].last_meal_time > data->time_to_die)
 		{
-			pthread_mutex_unlock(&data->meal_mutex);
-			pthread_mutex_lock(&data->death_mutex);
-			data->someone_died = 1;
-			pthread_mutex_unlock(&data->death_mutex);
-			pthread_mutex_lock(&data->print_mutex);
-			current_time = get_time() - data->start_time;
-			printf("%lld %d %s\n", current_time, data->philos[i].id, DIED_MSG);
-			pthread_mutex_unlock(&data->print_mutex);
-			return (1);
+    		pthread_mutex_unlock(&data->meal_mutex);
+    		pthread_mutex_lock(&data->death_mutex);
+    		data->someone_died = 1;
+    		pthread_mutex_unlock(&data->death_mutex);
+    		pthread_mutex_lock(&data->print_mutex);
+    		printf("%lld %d %s\n", current_time - data->start_time, 
+    		       data->philos[i].id, DIED_MSG);
+    		pthread_mutex_unlock(&data->print_mutex);
+    		return (1);
 		}
 		pthread_mutex_unlock(&data->meal_mutex);
 		i++;
--- philo_cld/philo.c
+++ philo_cld/philo.c
@@ -15,7 +15,7 @@ void	*philosopher_routine(void *arg)
 	}
 	
 	if (philo->id % 2 == 0)
-		smart_sleep(philo->data->time_to_eat / 2);
+		smart_sleep(philo->data->time_to_eat * 0.6);
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->death_mutex);
@@ -32,54 +32,52 @@ void	*philosopher_routine(void *arg)
 	return (NULL);
 }
 
-void	philo_eat(t_philo *philo)
-{
-	pthread_mutex_lock(&philo->data->death_mutex);
-	if (philo->data->someone_died)
-	{
-		pthread_mutex_unlock(&philo->data->death_mutex);
-		return ;
-	}
-	pthread_mutex_unlock(&philo->data->death_mutex);
-	
-	// Tek filozof özel durumu
-	if (philo->data->philo_count == 1)
-	{
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_MSG);
-		pthread_mutex_unlock(philo->left_fork);
-		return ;
-	}
-	
-	// Deadlock prevention: çift id'ler farklı sırada alır
-	if (philo->id % 2 == 1)
-	{
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_MSG);
-		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, FORK_MSG);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, FORK_MSG);
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_MSG);
-	}
-	
-	pthread_mutex_lock(&philo->data->meal_mutex);
-	philo->last_meal_time = get_time();
-	pthread_mutex_unlock(&philo->data->meal_mutex);
-	
-	print_status(philo, EAT_MSG);
-	smart_sleep(philo->data->time_to_eat);
-	
-	pthread_mutex_lock(&philo->data->meal_mutex);
-	philo->meals_eaten++;
-	pthread_mutex_unlock(&philo->data->meal_mutex);
-	
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
+void philo_eat(t_philo *philo) {
+    // Önce ölüm kontrolü
+    pthread_mutex_lock(&philo->data->death_mutex);
+    if (philo->data->someone_died) {
+        pthread_mutex_unlock(&philo->data->death_mutex);
+        return;
+    }
+    pthread_mutex_unlock(&philo->data->death_mutex);
+    
+    // TEK FİLOZOF DURUMU - MUTLAKA OLMALI!
+    if (philo->data->philo_count == 1) {
+        pthread_mutex_lock(philo->left_fork);
+        print_status(philo, FORK_MSG);
+        smart_sleep(philo->data->time_to_die + 1);
+        pthread_mutex_unlock(philo->left_fork);
+        return;
+    }
+    
+    // Deadlock prevention için mutex adresine göre sıralama
+    pthread_mutex_t *first_fork, *second_fork;
+    if (philo->left_fork < philo->right_fork) {
+        first_fork = philo->left_fork;
+        second_fork = philo->right_fork;
+    } else {
+        first_fork = philo->right_fork;
+        second_fork = philo->left_fork;
+    }
+    
+    pthread_mutex_lock(first_fork);
+    print_status(philo, FORK_MSG);
+    pthread_mutex_lock(second_fork);
+    print_status(philo, FORK_MSG);
+    
+    pthread_mutex_lock(&philo->data->meal_mutex);
+    philo->last_meal_time = get_time();
+    pthread_mutex_unlock(&philo->data->meal_mutex);
+    
+    print_status(philo, EAT_MSG);
+    smart_sleep(philo->data->time_to_eat);
+    
+    pthread_mutex_lock(&philo->data->meal_mutex);
+    philo->meals_eaten++;
+    pthread_mutex_unlock(&philo->data->meal_mutex);
+    
+    pthread_mutex_unlock(second_fork);
+    pthread_mutex_unlock(first_fork);
 }
 
 void	philo_sleep(t_philo *philo)
